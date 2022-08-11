cd85523f3b0c7c1805f26741f50d46f68d253a5f
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-08-11 10:31:01 +0900

    ロングノーツ生成コードの作成1

--- Assets/Scripts/NotesManager.cs
+++ Assets/Scripts/NotesManager.cs
@@ -37,6 +37,8 @@ public class NotesManager : MonoBehaviour
 
     private GameObject noteObj;
 
+    private float noteScale = 1.0f;
+
     void OnEnable() {
         noteNum = 0;
         title = "Emargence Vibe2";
@@ -49,24 +51,54 @@ public class NotesManager : MonoBehaviour
 
         noteNum = inputJson.notes.Length;
 
-        for (int i = 0; i < inputJson.notes.Length; i++)
-        {
+        for (int i = 0; i < inputJson.notes.Length; i++) {
             noteObj = noteObjs[inputJson.notes[i].block];
 
             float interval = 60 / (inputJson.BPM * (float)inputJson.notes[i].LPB);
             float beatSec = interval * (float)inputJson.notes[i].LPB;
             float time = (beatSec * inputJson.notes[i].num / (float)inputJson.notes[i].LPB) + inputJson.offset * 0.01f;
+
             NotesTime.Add(time);
             LaneNum.Add(inputJson.notes[i].block);
             NoteType.Add(inputJson.notes[i].type);
 
             float z = NotesTime[i] * NotesSpeed;
 
+            // ロングノーツの場合、専用ジェネレーターにかける
+            if (inputJson.notes[i].type == 2) {
+                // 始点のZ座標
+                float startZ = z;
+                // 終点のZ座標
+                float enInterval = 60 / (inputJson.BPM * (float)inputJson.notes[i].notes);
+
+                // 始点のX座標
+                Vector3 startX = new Vector3(inputJson.notes[i].block - 3.56f, 0.55f, z);
+                // 終点のX座標
+                Vector3 endX = new Vector3(inputJson.notes[i].notes - 3.56f, 0.55f, z);
+
+                GenerateLongNote(startX, endX, noteObj);
+            }
             NotesObj.Add(Instantiate(noteObj, new Vector3(inputJson.notes[i].block - 3.56f, 0.55f, z), Quaternion.identity));
         }
-        
-        // for (int i = 0; i < 46; i++) {
-        //     Debug.Log(NotesTime[i]);
-        // }
+    }
+
+    private void GenerateLongNote(Vector3 startX, Vector3 endX, GameObject noteObj) {
+        // メッシュの生成
+        Mesh mesh = new Mesh();
+        noteObj.GetComponent<MeshFilter>().mesh = mesh;
+
+        Vector3[] Vertices = new Vector3[4];
+        int[] triangles = new int[6];
+
+        Vertices[0] = startX + new Vector3(-noteScale / 2, 0, 0);
+        Vertices[1] = startX + new Vector3(noteScale / 2, 0, 0);
+        Vertices[2] = endX + new Vector3(-noteScale / 2, 0, 0);
+        Vertices[3] = endX + new Vector3(noteScale / 2, 0, 0);
+
+        triangles = new int[6] {0,2,1,3,1,2};
+
+        mesh.vertices = Vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
     }
 }
