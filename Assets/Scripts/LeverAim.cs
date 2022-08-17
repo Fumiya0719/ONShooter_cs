8dadf0779ca53de6bacde97740652d86a746cbc2
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-08-17 22:02:02 +0900

    Sideノーツのスクリプトに通常ノーツも統一

---
+++ Assets/Scripts/LeverAim.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Update is called once per frame
+    // void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.Space)) {
+    //         start = true;
+    //     }
+
+    //     if (start) {
+    //         // ノーツの移動
+    //         transform.position -= transform.forward * Time.deltaTime * HighSpeed;
+    //     }
+    // }
+}
